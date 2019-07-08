e3bb853e3197ae64ee45190dc01bb524472a45ff
Daniel Innes <Daniel.innes@cira.ca>
2019-07-08 09:23:57 -0400

    Revert "Move to legacyEsConfig"
    This reverts commit 5190272580cda6f8ce899515bc3a46c3d926e3f1.

--- kibana-auth/index.js
+++ kibana-auth/index.js
@@ -5,10 +5,10 @@ export default function (kibana) {
     /* These identify the application in the navigation panel */
     const DEV_APPS_ID = [
         'apm', 'kibana:dev_tools', 'monitoring', 'kibana:management', 'timelion', 'ml', 'infra',
-        'kibana-prometheus-exporter', 'uptime', 'siem'
+        'kibana-prometheus-exporter', 'uptime'
     ];
     /* These identify core applications in the URL */
-    const DEV_APPS_CORE_URL = ['/dev_tools', '/management', '/uptime', '/monitoring', '/graph', '/apm', '/siem'];
+    const DEV_APPS_CORE_URL = ['/dev_tools', '/management', '/uptime', '/monitoring', '/graph', '/apm'];
 
     return new kibana.Plugin({
         name: 'kibana-auth',
