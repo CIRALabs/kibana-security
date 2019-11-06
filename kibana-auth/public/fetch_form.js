3c6e94735fef994558278b8f6044bb04d000a95e
Daniel Innes <Daniel.innes@cira.ca>
2019-11-06 11:16:09 -0500

    Improved alerts

---
+++ kibana-auth/public/fetch_form.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
+  Array.from(document.getElementsByClassName("fetch-form")).forEach((element) => {
+    element.onsubmit = async (e) => {
+      e.preventDefault();
+      const params = new URLSearchParams([...new FormData(e.target).entries()]);
+      fetch(document.getElementById(e.target.id).action, {
+        method: document.getElementById(e.target.id).method,
+        body: params,
+        headers: {"kbn-xsrf": "reporting"}
+      }).then(function (response) {
+        if (response.ok) {
+          response.text().then((text) => {
+            location.replace(text)
+          })
+        } else {
+          response.json().then((json) => {
+            if (response.status === 403 && document.getElementById("alert").classList.contains("hidden")) {
+              document.getElementById("alert").classList.remove("hidden");
+              document.getElementById("alert-message").innerHTML = json.message;
+            } else {
+              console.log(json.type, json.message)
+            }
+          })
+        }
+      })
+    }
+  })
+});
